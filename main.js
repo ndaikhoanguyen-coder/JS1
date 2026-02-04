aa962f3578569a671f9c00aac25cc8f6b4104cbe
Khoa Nguyen <n.daikhoanguyen@gmail.com>
2026-02-04 14:51:12 +0700

    test4

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 // alert("Hello");
+
+let userName;
 prompt("Enter Name");
 var myName = "Nguyen";
 var sayHi = "Toi ten la Nguyen";
 console.log("My name is:" + myName);
 console.log(sayHi);
+userName = prompt("Enter Name");
+console.log("Tên của bạn: " + userName);
